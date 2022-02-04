1f63bb622a16c5f49b0bed317cb31092d36cb4b7
Santos <luan.de.s.santos@accenture.com>
2022-02-04 00:01:59 -0300

    versao final do projeto

---
+++ src/main/java/com/financial/schedule/com/br/financialSchedule/FinancialScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.financial.schedule.com.br.financialSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financial/schedule/com/br/financialSchedule/FinancialScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial.schedule.com.br.financialSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
